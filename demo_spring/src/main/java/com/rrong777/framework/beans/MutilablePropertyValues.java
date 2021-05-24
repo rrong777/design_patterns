922aafda1cdf4e0a25a830a5f5da666b1351b7c1
wuqr <wql789@qq.com>
2021-05-24 23:37:00 +0800

    手写Spring

---
+++ demo_spring/src/main/java/com/rrong777/framework/beans/MutilablePropertyValues.java
@@ -0,0 +1,9 @@
+package com.rrong777.framework.beans;
+
+/**
+ * 用户用来存储和管理多个PropertyValue的对象
+ * 一个bean标签可以有多个property标签，每一个property标签会被封装成一个PropertyValue对象，多个的话就是多个PropertyValue对象
+ * 这里就定义一个MutilablePropertyValues 对PropertyValue对象进行管理。迭代器模式，管理多个PropertyValue对象，可以迭代的。
+ */
+public class MutilablePropertyValues {
+}
